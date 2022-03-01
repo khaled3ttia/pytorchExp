bdf7210e4bc59015a5f9ba69a1f0e2baed1ab3ff
khaled3ttia <khaled.3ttia@gmail.com>
2022-03-01 12:48:43 -0600

    use sets instead of lists when generating the dataset to ensure no duplicates are present

--- generate_dataset.py
+++ generate_dataset.py
@@ -10,20 +10,26 @@
 
 import random
 
-NUM_FEATURES = 12 # For now, it must be a multiple of 4
+NUM_FEATURES = 20 # For now, it must be a multiple of 4
 NUM_RECORDS = 1200 # can be any number <= 2^12
 DATASET_FILE='data' # dataset filename
 
+# A function to add an item to the set
+# returns true if the item was actually added,
+# otherwise false
+def setAdd(s, item):
+    l1 = len(s)
+    s.add(item)
+    return len(s) > l1
 
-# This is where the class labels are saved
-output = []
 
 # Stores input instances
-records = []
+records = set()
 
 # generate random input
 for i in range(NUM_RECORDS):
-    records.append([])
+    #records.append([])
+    tmp = []
 
     # generate a random number between 0 and 1
     # that represents a probability
@@ -37,10 +43,10 @@ for i in range(NUM_RECORDS):
         for j in range(int(NUM_FEATURES/2)):
             leftHalf.append(random.randint(0,9))
         rightHalf = leftHalf[::-1]
-        records[i] = leftHalf + rightHalf
+        tmp = leftHalf + rightHalf
 
-        # The label for this case should be 0
-        output.append(0)
+        # The label for this class is 0
+        setAdd(records, tuple(tmp) + tuple([0]))
 
     elif (prob > 0.25 and prob < 0.5):
         # Make the record consists of two identical
@@ -48,10 +54,10 @@ for i in range(NUM_RECORDS):
         leftHalf = []
         for j in range(int(NUM_FEATURES/2)):
             leftHalf.append(random.randint(0,9))
-        records[i] = leftHalf + leftHalf
+        tmp = leftHalf + leftHalf
 
         # The label for this case should be 1
-        output.append(1)
+        setAdd(records, tuple(tmp) + tuple([1]))
 
     elif (prob > 0.5 and prob < 0.75):
         # Make the record consist of 4 quarters
@@ -66,10 +72,11 @@ for i in range(NUM_RECORDS):
         q1_r = q1[::-1]
         q2_r = q2[::-1]
 
-        records[i] = q1 + q1_r + q2 + q2_r
+        tmp = q1 + q1_r + q2 + q2_r
 
         # The label for this case should be 2
-        output.append(2)
+        setAdd(records, tuple(tmp) + tuple([2]))
+        
     else:
         # Make the record consists of 4 quarters
         # Q1 : Q1 : Q2 : Q2
@@ -79,24 +86,24 @@ for i in range(NUM_RECORDS):
             q1.append(random.randint(0,9))
             q2.append(random.randint(0,9))
 
-        records[i] = q1 + q1 + q2 + q2
+        tmp = q1 + q1 + q2 + q2
 
         # The label for this case should be 3
-        output.append(3)
+        setAdd(records, tuple(tmp) + tuple([3]))
 
 
 # sanity check
 assert(len(records) == NUM_RECORDS)
-assert(len(records[0]) == NUM_FEATURES)
-assert(len(output) == NUM_RECORDS)
+
+for record in records:
+    assert(len(record) == NUM_FEATURES+1)
 
 print('Everything looks good, saving dataset...')
 
 with open(DATASET_FILE+'.csv','w') as wf:
-    for i in range(len(records)):
-        for j in range(len(records[i])):
-            wf.write(f'{records[i][j]},')
-        #wf.write('\n')
-        wf.write(f'{output[i]}\n')
+    for record in records:
+        for i in range(len(record)-1):
+            wf.write(f'{record[i]},')
+        wf.write(f'{record[len(record)-1]}\n')
 
 print(f'Dataset file successfully written to {DATASET_FILE}.csv')
